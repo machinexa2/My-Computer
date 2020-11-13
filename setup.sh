1e8c6ae22fd6ecd93c0b2ba21875c666dad2d690
root <machinexa>
2020-11-13 18:23:38 +0545

    06:23:38 PM 11/13/2020

--- setup.sh
+++ setup.sh
@@ -3,19 +3,17 @@ mkdir Softwares 2>/dev/null
 read -p "Enter password: " passmepass
 
 github_password=":""$passmepass""@"
-dontclone='I-am-New-To-Bug-Bounty|Saved-Games'
+zopiclone='I-am-New-To-Bug-Bounty|Saved-Games'
 
-# Clone repositories
-cat README.md  | egrep -v $dontclone |  awk -F '(' '{print $2}' | awk -F ')**' '{print $1}' | sort -u | while read repository
+cat README.md  | egrep -v $zopiclone |  awk -F '(' '{print $2}' | awk -F ')**' '{print $1}' | sort -u | while read repository
 do
 	if [ ! -z "$repository" ]; then
 		git clone `echo "$repository"".git" | sed s/'github.com'/'machinexa2:@github.com'/g | sed s/':@'/$github_password/g`
 	fi
 done
-
-# Setup Kali
 cd My-Knowledge/KaliSetup/
 	bash apt.data
 	bash pip.data
 	bash tool.data
 cd $CURRENT
+bash extra.sh
